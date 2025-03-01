d0715e2c2054e237f8c82d7f22e46e253ddb312b
Moldiir <moldir.umrali@narxoz.kz>
2025-03-01 10:28:39 +0500

    Added Room.java and NPC.java

--- src/Prototype/NPC.java
+++ src/Prototype/NPC.java
@@ -1,2 +1,36 @@
-package src.Prototype;public class NPC {
+package src.Prototype;
+
+public class NPC implements CloneableGameEntity {
+    private String name;
+    private String description;
+    private int health;
+
+    public NPC(String name, String description, int health) {
+        this.name = name;
+        this.description = description;
+        this.health = health;
+    }
+
+    @Override
+    public NPC cloneEntity() {
+        return new NPC(this.name, this.description, this.health);
+    }
+
+    // Setter methods to modify cloned NPC
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return "NPC name: '" + name + "', description: '" + description + "', health: " + health + ";";
+    }
 }
--- src/Prototype/Room.java
+++ src/Prototype/Room.java
@@ -1,2 +1,30 @@
-package src.Prototype;public class Room {
+package src.Prototype;
+
+public class Room implements CloneableGameEntity {
+    private String name;
+    private String description;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public Room cloneEntity() {
+        return new Room(this.name, this.description);
+    }
+
+    // Setter methods to modify cloned Room
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Room name: '" + name + "', description: '" + description + "';";
+    }
 }
